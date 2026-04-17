c02b8823f4f2a7423736b621b7283bf93bbb2731
Renato Gonzales <rent.gonz@gmail.com>
2026-04-17 17:21:15 -0500

    agregado neovim plugins

---
+++ nvim/init.lua
@@ -0,0 +1,34 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("config.lazy")
+
+vim.opt.title = true             -- set title
+vim.opt.number = true            -- set nu
+vim.opt.mouse = ""               -- set mouse=
+vim.opt.incsearch = true         -- set incsearch
+vim.opt.background = "dark"      -- set background=dark
+vim.opt.shiftwidth = 4           -- set shiftwidth=4
+vim.opt.softtabstop = 2          -- set softtabstop=2
+vim.opt.expandtab = true         -- set expandtab
+vim.opt.smarttab = true          -- set smarttab
+vim.opt.clipboard = "unnamedplus" -- set clipboard=unnamedplus
+vim.cmd.colorscheme "catppuccin-nvim"
+
+local config = {
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+}
+
+require("nvim-tree").setup(config)
+
